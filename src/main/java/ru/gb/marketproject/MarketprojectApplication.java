0ee0b78642e690f457ad99e92b09d6dd56174814
ramil <pashaevra@gmail.com>
2021-06-30 20:04:54 +0300

    Initial commit

---
+++ src/main/java/ru/gb/marketproject/MarketprojectApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.marketproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/marketproject/MarketprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.marketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
